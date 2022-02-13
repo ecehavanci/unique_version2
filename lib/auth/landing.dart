e4964f9270c8692d61583edd02407ad1eea1c514
ecehavanci <ecehavanci@gmail.com>
2022-02-13 23:49:30 +0300

    sign in page overflex issue solved, sign up page completed

--- lib/auth/landing.dart
+++ lib/auth/landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unique_version2/auth/signup.dart';
 import 'package:unique_version2/widgets/custom_text_button.dart';
 import 'package:unique_version2/widgets/custom_text_input.dart';
 import 'package:unique_version2/widgets/sign_in_button.dart';
@@ -32,149 +33,146 @@ class _LandingPageState extends State<LandingPage> {
             builder: (BuildContext context, BoxConstraints constraints) => Center(
               child: Column(
                 children: [
-                  Spacer(flex: 3),
+                  Spacer(flex: 4),
                   Text(
                     "Welcome To Unique",
                     style: textTheme.subtitle1!.copyWith(fontSize: 30),
-                  ),Container(
+                  ),
+                  Container(
                     child: Image(
-                      width: width*0.45,
+                      width: width * 0.45,
                       image: AssetImage('asset/human_pet.png'),
                       fit: BoxFit.fill, //fill type of image inside aspectRatio
                     ),
                   ),
                   //Spacer(flex: 2),
-                  Container(
-                    width: width,
-                    height: height * 0.70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(100), topRight: Radius.circular(100)),
-                      color: AppColors.white,
-                    ),
-                    child: Column(
-                      children: [
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              left: width * 0.1,
-                            ),
-                            child: Text(
-                              "Sign In",
-                              style: textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold, fontSize: 24),
-                            ),
-                          ),
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
-                            child: Text(
-                              "Email",
-                              style: textTheme.headline4!.copyWith(
-                                color: AppColors.mediumGray,
+                  Expanded(
+                    flex: 20,
+                    child: Container(
+                      width: width,
+                      height: height * 0.70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(100), topRight: Radius.circular(100)),
+                        color: AppColors.white,
+                      ),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            SizedBox(height: height * 0.03),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                  left: width * 0.1,
+                                ),
+                                child: Text(
+                                  "Sign In",
+                                  style: textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold, fontSize: 24),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: width * 0.1,
-                            right: width * 0.1,
-                            bottom: height * 0.03,
-                          ),
-                          child: CustomTextInput(
-                            label: 'email',
-                            controller: mailController,
-                            keyboardType: TextInputType.emailAddress,
-                            prefixIcon: Padding(
-                              padding: const EdgeInsets.only(right: 10),
-                              child: Icon(Icons.email),
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
-                            child: Text(
-                              "Password",
-                              style: textTheme.headline4!.copyWith(
-                                color: AppColors.mediumGray,
+                            SizedBox(height: height * 0.03),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Email",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: width * 0.1,
-                            right: width * 0.1,
-                          ),
-                          child: CustomTextInput(
-                            label: 'password',
-                            controller: passwordController,
-                            keyboardType: TextInputType.visiblePassword,
-                            prefixIcon: Padding(padding: EdgeInsets.only(right: 10), child: Icon(Icons.vpn_key)),
-                            suffixIcon: IconButton(
-                              icon: isKeyOpen
-                                  ? Icon(
-                                      Icons.remove_red_eye,
-                                      color: Colors.grey,
-                                    )
-                                  : Icon(
-                                      Icons.remove_red_eye,
-                                      color: Colors.blue,
-                                    ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                                bottom: height * 0.03,
+                              ),
+                              child: CustomTextInput(
+                                label: 'email',
+                                controller: mailController,
+                                keyboardType: TextInputType.emailAddress,
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: Icon(Icons.email),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Password",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                              ),
+                              child: CustomTextInput(
+                                label: 'password',
+                                controller: passwordController,
+                                keyboardType: TextInputType.visiblePassword,
+                                prefixIcon: Padding(padding: EdgeInsets.only(right: 10), child: Icon(Icons.vpn_key)),
+                                suffixIcon: IconButton(
+                                  icon: isKeyOpen
+                                      ? Icon(
+                                          Icons.remove_red_eye,
+                                          color: Colors.grey,
+                                        )
+                                      : Icon(
+                                          Icons.remove_red_eye,
+                                          color: Colors.blue,
+                                        ),
+                                  onPressed: () {
+                                    print(isKeyOpen);
+                                    setState(() {
+                                      isKeyOpen = !isKeyOpen;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: height * 0.03),
+                            SignInButton(
+                              title: "Sign In",
+                              width: width * 0.40,
+                              height: 50,
                               onPressed: () {
-                                print(isKeyOpen);
-                                setState(() {
-                                  isKeyOpen = !isKeyOpen;
-                                });
+                                print("go to home page");
+                              },
+                              color: AppColors.yellow,
+                              textStyle: textTheme.button!.copyWith(
+                                color: AppColors.orange,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: height * 0.03),
+                            Text(
+                              "Not a member? Join us now!",
+                              style: textTheme.subtitle1,
+                            ),
+                            CustomTextButton(
+                              title: "Sign Up",
+                              style:
+                                  textTheme.subtitle1!.copyWith(color: AppColors.orange, fontWeight: FontWeight.bold),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUpPage()));
                               },
                             ),
-                          ),
+                            SizedBox(height: height * 0.03),
+                          ],
                         ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        SignInButton(
-                          title: "Sign In",
-                          width: width * 0.40,
-                          height: 50,
-                          onPressed: () {
-                            print("go to home page");
-                          },
-                          color: AppColors.yellow,
-                          textStyle: textTheme.button!.copyWith(
-                            color: AppColors.orange,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Spacer(
-                          flex: 1,
-                        ),
-                        Text(
-                          "Not a member? Join us now!",
-                          style: textTheme.subtitle1,
-                        ),
-                        CustomTextButton(
-                          title: "Sign Up",
-                          style: textTheme.subtitle1!.copyWith(color: AppColors.orange, fontWeight: FontWeight.bold),
-                          onPressed: () {
-                            print("go to sign up page");
-                          },
-                        ),
-                        Spacer(
-                          flex: 1,
-                        )
-                      ],
+                      ),
                     ),
                   ),
                 ],
--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,10 +1,238 @@
 import 'package:flutter/material.dart';
+import 'package:unique_version2/widgets/custom_text_button.dart';
+import 'package:unique_version2/widgets/custom_text_input.dart';
+import 'package:unique_version2/widgets/sign_in_button.dart';
 
-class SignUpPage extends StatelessWidget {
+import '../colors.dart';
+
+class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool isKeyOpen = false;
+  TextEditingController mailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController phoneController = new TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final textTheme = Theme.of(context).textTheme;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final themeData = Theme.of(context).backgroundColor;
+
+    return Scaffold(
+      body: Container(
+        color: themeData,
+        child: Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) => Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Sign Up",
+                    style: textTheme.subtitle1!.copyWith(fontSize: 24),
+                  ),
+                  Text(
+                    "Create a new account and find your pet's needs",
+                    style: textTheme.subtitle1!.copyWith(fontSize: 13),
+                  ),
+                  Container(
+                    child: Image(
+                      width: width * 0.45,
+                      image: AssetImage('asset/human_pet_game.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      width: width,
+                      height: height * 0.70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(100), topRight: Radius.circular(100)),
+                        color: AppColors.white,
+                      ),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            SizedBox(height: height * 0.03),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Name",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                                bottom: height * 0.03,
+                              ),
+                              child: CustomTextInput(
+                                label: 'name',
+                                controller: nameController,
+                                keyboardType: TextInputType.name,
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: Icon(Icons.account_circle_rounded),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Email",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                                bottom: height * 0.03,
+                              ),
+                              child: CustomTextInput(
+                                label: 'email',
+                                controller: mailController,
+                                keyboardType: TextInputType.emailAddress,
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: Icon(Icons.email),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Phone",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                                bottom: height * 0.03,
+                              ),
+                              child: CustomTextInput(
+                                label: 'phone',
+                                controller: phoneController,
+                                keyboardType: TextInputType.phone,
+                                prefixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 10),
+                                  child: Icon(Icons.phone),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: EdgeInsets.only(left: width * 0.15, bottom: 10),
+                                child: Text(
+                                  "Password",
+                                  style: textTheme.headline4!.copyWith(
+                                    color: AppColors.mediumGray,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: width * 0.1,
+                                right: width * 0.1,
+                              ),
+                              child: CustomTextInput(
+                                label: 'password',
+                                controller: passwordController,
+                                keyboardType: TextInputType.visiblePassword,
+                                prefixIcon: Padding(padding: EdgeInsets.only(right: 10), child: Icon(Icons.vpn_key)),
+                                suffixIcon: IconButton(
+                                  icon: isKeyOpen
+                                      ? Icon(
+                                          Icons.remove_red_eye,
+                                          color: Colors.grey,
+                                        )
+                                      : Icon(
+                                          Icons.remove_red_eye,
+                                          color: Colors.blue,
+                                        ),
+                                  onPressed: () {
+                                    print(isKeyOpen);
+                                    setState(() {
+                                      isKeyOpen = !isKeyOpen;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: height * 0.03),
+                            SignInButton(
+                              title: "Sign Up",
+                              width: width * 0.40,
+                              height: 50,
+                              onPressed: () {
+                                print("go to home page");
+                              },
+                              color: AppColors.lightblue,
+                              textStyle: textTheme.button!.copyWith(
+                                color: AppColors.orange,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: height * 0.03),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Already have an account? ",
+                                  style: textTheme.subtitle1,
+                                ),
+                                CustomTextButton(
+                                  title: "Sign in Now!",
+                                  style:
+                                      textTheme.subtitle1!.copyWith(color: AppColors.orange, fontWeight: FontWeight.bold),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                            //SizedBox(height: height*0.05),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
